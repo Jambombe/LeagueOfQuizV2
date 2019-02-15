77dc563deee4ebef60a9ea8cd1585781d0019db4
Jambombe <flechebleue38@hotmail.com>
2019-02-15 14:21:38 +0100

    Start quiz methods using Swal

--- public/resources/js/starting-screen.js
+++ public/resources/js/starting-screen.js
@@ -1,3 +1,25 @@
-function startingScreenInit(){
-    console.log('oui');
+async function startingScreenInit(startingScreen){
+    startingScreen = startingScreen.parentNode;
+    let countdownElem = document.getElementById('countdown').children[0];
+    console.log(startingScreen);
+
+    await countdown(3, countdownElem);
+
+    startingScreen.style.display = 'none';
+}
+
+/**
+ * Compte à rebours de time à 0, affichant le temps restant dans l'element e
+ * @param time sec
+ * @param e element
+ */
+async function countdown(time, e)
+{
+    await setTimeout(function () {}, 5000);
+}
+
+function setDelay() {
+    setTimeout(function(){
+        console.log('oui');
+    }, 1000);
 }
